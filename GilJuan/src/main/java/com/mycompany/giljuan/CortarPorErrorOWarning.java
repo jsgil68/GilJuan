4954e391213b6e52eee8e5160454012ae287a819
jsgil68 <113575964+jsgil68@users.noreply.github.com>
2022-10-20 13:48:37 -0500

    Add files via upload
    v.1 Realizado en el tiempo estipulado en la clase

---
+++ GilJuan/src/main/java/com/mycompany/giljuan/CortarPorErrorOWarning.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.giljuan;
+
+import java.util.List;
+
+/**
+ *
+ * @author sala4
+ */
+public class CortarPorErrorOWarning {
+    
+    public void ejecutar(){
+        
+    }
+}
---
+++ GilJuan/src/main/java/com/mycompany/giljuan/Tarea.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.giljuan;
+
+import java.util.Date;
+
+/**
+ *
+ * @author sala4
+ */
+public class Tarea {
+    boolean habilitada;
+    boolean deshabilitarSiFalla;
+  
+    public void ejecutar() {
+       
+}
+    public void doEjecutar(){
+        
+    }
+    
+    public Resultado ultimaEjecucion(){
+        
+    return null; 
+    }
+    
+    public void vecesQueSeEjecuta(int Date, int Dato){
+        
+    }
+    
+    public void vecesQueDioError(int Date, int Dato){
+        
+    }
+    
+}
